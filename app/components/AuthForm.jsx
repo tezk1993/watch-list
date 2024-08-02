c8e8fe5e1af5655231d2c0271ab7e4110287639c
busser1993 <sickogagi@msn.com>
2024-08-02 14:49:02 +0200

    Removed local location

--- app/components/AuthForm.jsx
+++ app/components/AuthForm.jsx
@@ -12,7 +12,7 @@ const supabase = createClientComponentClient();
         view="magic_link"
         showLinks={false}
         providers={[]}
-        redirectTo="http://localhost:3000/auth/callback"
+        redirectTo="http://watch-list-two.vercel.app/auth/callback"
         appearance={
             {
                 theme: 'dark',
